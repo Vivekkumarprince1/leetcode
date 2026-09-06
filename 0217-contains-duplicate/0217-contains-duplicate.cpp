a74937da16c76c02a323f8b4262974850e8c6cd1
VIVEK KUMAR <164745187+Vivekkumarprince1@users.noreply.github.com>
2026-09-06 22:55:32 +0530

    Time: 77 ms (33.57%), Space: 111.2 MB (73.89%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,24 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         unordered_set<int>mp;
+//         for(int num:nums){
+//             if(mp.count(num))return true;
+//             mp.insert(num);
+//         }
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int>mp;
-        for(int num:nums){
-            if(mp.count(num))return true;
-            mp.insert(num);
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp[nums[i]] > 0) {
+                return true;
+            }
+            mp[nums[i]]++;
         }
         return false;
     }
